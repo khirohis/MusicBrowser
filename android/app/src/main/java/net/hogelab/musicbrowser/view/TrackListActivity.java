bef7be515dc0080d6d6bb1db849643dc407b088e
Hirohisa Kobayasi <hogelab.net@gmail.com>
2016-05-09 14:40:00 +0900

    transparent status bar

--- android/app/src/main/java/net/hogelab/musicbrowser/view/TrackListActivity.java
+++ android/app/src/main/java/net/hogelab/musicbrowser/view/TrackListActivity.java
@@ -3,6 +3,7 @@ package net.hogelab.musicbrowser.view;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
@@ -73,6 +74,11 @@ public class TrackListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        }
 
         mViewModel = new TrackListRootViewModel(this, null);
 
